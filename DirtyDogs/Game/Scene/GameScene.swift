105e880d6d05906677fe6d186379b8696f9d277f
Isadora Guerra <isadoraguerra@Isadoras-MacBook-Pro.local>
2025-12-02 14:18:46 -0300

    update

--- DirtyDogs/Game/Scene/GameScene.swift
+++ DirtyDogs/Game/Scene/GameScene.swift
@@ -154,7 +154,7 @@ public class GameScene: SKScene {
         for entity in entities {
             guard let node = entity.component(ofType: GKSKNodeComponent.self)?.node else { continue }
             
-            let collectionLineY = frame.minY + 280
+            let collectionLineY = frame.minY + 250
                         
             if node.position.y < collectionLineY && isInventoryFull == false {
                 collect(entity: entity as! GameEntity)
