e43c85e1ad7999eedf9dc8383fd0327f3086ca48
Isadora Guerra <isadoraguerra@Isadoras-MacBook-Pro.local>
2025-12-02 16:34:10 -0300

    working

--- DirtyDogs/Game/Entities/Tint.swift
+++ DirtyDogs/Game/Entities/Tint.swift
@@ -28,33 +28,13 @@ public class Tint: GKEntity, GameEntity {
         let container = SKNode()
         container.name = "tint"
 
-//        let base = SKShapeNode(circleOfRadius: sizeBase)
-//        base.fillColor = UIColor(red: 0.36, green: 0.20, blue: 0.04, alpha: 1.0) // marrom escuro
-//        base.strokeColor = base.fillColor.withAlphaComponent(0.7)
-//        base.position = CGPoint(x: 0, y: 0)
-//
-//        let middle = SKShapeNode(circleOfRadius: sizeBase * 0.7)
-//        middle.fillColor = base.fillColor
-//        middle.strokeColor = base.strokeColor
-//        middle.position = CGPoint(x: 0, y: sizeBase * 0.8)
-//
-//        let top = SKShapeNode(circleOfRadius: sizeBase * 0.45)
-//        top.fillColor = base.fillColor
-//        top.strokeColor = base.strokeColor
-//        top.position = CGPoint(x: 0, y: sizeBase * 1.45)
-
         let bomb = SKSpriteNode(imageNamed: "Tint")
         bomb.size = CGSize(width: sizeBase * 2, height: sizeBase * 2)
         bomb.name = "tint"
         container.addChild(bomb)
 
-//        let texture = bomb.texture!
-//        container.physicsBody = SKPhysicsBody(texture: texture, size: bomb.size)
         container.physicsBody = SKPhysicsBody(circleOfRadius: sizeBase)
 
-//        container.addChild(base)
-//        container.addChild(middle)
-//        container.addChild(top)
         container.physicsBody = SKPhysicsBody(circleOfRadius: sizeBase)
         container.physicsBody?.affectedByGravity = false
         container.physicsBody?.categoryBitMask = PhysicsCategory.parcel
