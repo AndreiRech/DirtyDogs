66f5fce4dfef46ba62356848a08e6832970a3607
AndreiRech <andreirech22@outlook.com>
2025-12-08 14:34:13 -0300

    feat: create the tutorial view

--- DirtyDogs/Screens/Tutorial/TutorialView.swift
+++ DirtyDogs/Screens/Tutorial/TutorialView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct TutorialView: View {
-    
     @Environment(\.dismiss) var dismiss
+    @State var viewModel: TutorialViewModelProtocol
     
     var body: some View {
-        VStack (alignment: .center) {
+        VStack(spacing: 0) {
+            Spacer()
+            
             HStack {
                 Spacer()
+                
                 Button {
                     dismiss()
                 } label: {
@@ -22,27 +25,35 @@ struct TutorialView: View {
                         .resizable()
                         .scaledToFill()
                         .frame(width: 48, height: 48)
-                        .padding()
+                        .padding(.horizontal, 16)
                 }
             }
-            CardView(
-                imageName: "Grass-Light",
-                totalPages: 3,
-                atualPage: 2
-            )
+            .offset(y: 40)
+            
+            TabView(selection: $viewModel.currentPage) {
+                ForEach(1...viewModel.numberOfPages, id: \.self) { index in
+                    CardView(
+                        imageName: viewModel.getPage(for: index),
+                        totalPages: viewModel.numberOfPages,
+                        atualPage: index
+                    )
+                    .tag(index)
+                }
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .offset(y: -60)
             
             Spacer()
-            
         }
         .background(
             MovingBackground()
         )
         .navigationBarBackButtonHidden(true)
         .toolbar(.hidden)
-        .padding(.top, 32)
+        .padding(.horizontal, 16)
     }
 }
 
 #Preview {
-    TutorialView()
+    TutorialView(viewModel: TutorialViewModel())
 }
