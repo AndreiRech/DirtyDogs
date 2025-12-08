0ec9e19cf5f2971f3d8778bcdbd42b29aa18114a
Júlia Fazenda <juliafazendaruiz@gmail.com>
2025-12-08 17:50:53 -0300

    fix: fix launch screen

--- DirtyDogs/Game/Scene/GameScene.swift
+++ DirtyDogs/Game/Scene/GameScene.swift
@@ -62,6 +62,7 @@ public class GameScene: SKScene {
         setupBorders()
         
         _ = gridManager.createMap(horizontal: 3, vertical: 4)
+        
     }
     
     public override func update(_ currentTime: TimeInterval) {
