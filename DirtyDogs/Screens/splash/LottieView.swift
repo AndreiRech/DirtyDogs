df9bcdaf33ee571ff2522dc148e535daa91b80c7
Isadora Guerra <isadoraguerra@Isadoras-MacBook-Pro.local>
2025-12-08 14:23:56 -0300

    last version

--- DirtyDogs/Screens/splash/LottieView.swift
+++ DirtyDogs/Screens/splash/LottieView.swift
@@ -12,39 +12,45 @@ struct LottieView: UIViewRepresentable {
     let name: String
     let loopMode: LottieLoopMode
     let onComplete: (() -> Void)?
-
+    
     private let animationView = LottieAnimationView()
-
+    
     func makeUIView(context: Context) -> UIView {
         let view = UIView()
-
+        
         let animation = LottieAnimation.named(name)
         animationView.animation = animation
         animationView.contentMode = .scaleAspectFill
         animationView.loopMode = loopMode
         
-        animationView.play { finished in
-                    if finished {
-                        onComplete?()
-                    }
-                }
-
+        if let animation {
+            let totalFrames = animation.endFrame
+            let fps = animation.framerate
+            
+            let framesToCut = Int(fps * 3.0)
+            let visibleEndFrame = max(0, totalFrames - CGFloat(framesToCut))
+            
+            animationView.play(fromFrame: 0, toFrame: visibleEndFrame) { finished in
+                if finished { onComplete?() }
+            }
+        }
+        
         animationView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(animationView)
-
+        
         NSLayoutConstraint.activate([
             animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
             animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
             animationView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             animationView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
-
+        
         return view
     }
-
+    
     func updateUIView(_ uiView: UIView, context: Context) {}
 }
 
 #Preview {
     LottieView(name: "splash screen", loopMode: .playOnce, onComplete: nil)
-               }
+}
--- DirtyDogs/Screens/splash/SplashMainView.swift
+++ DirtyDogs/Screens/splash/SplashMainView.swift
@@ -12,23 +12,18 @@ import Lottie
 struct SplashToPawsView: View {
     @State private var showHome = false
     @State private var showBackground = false
-    
-    let timer = Timer.publish(every: 0.4, on: .main, in: .common).autoconnect()
-    let batchSize = 10
-    let totalImages = 100
-    
+        
     var body: some View {
         ZStack {
             LottieView(
                 name: "splash screen",
                 loopMode: .playOnce,
                 onComplete: {
-                    //                    showHome = true
                     withAnimation(.easeIn(duration: 0.2)) {
                         showBackground = true
                     }
                     
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
                         withAnimation {
                             showHome = true
                         }
@@ -41,6 +36,8 @@ struct SplashToPawsView: View {
                 Image(.fundo)
                     .resizable()
                     .scaledToFill()
+                    .ignoresSafeArea()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
             if showHome {
                 MenuView(
