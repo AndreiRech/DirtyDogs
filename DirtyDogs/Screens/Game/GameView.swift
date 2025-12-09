1d1f98355ee279f25693ad587804e708e442b2cb
Júlia Fazenda <juliafazendaruiz@gmail.com>
2025-12-09 17:54:52 -0300

    feat: digging animation by tile color

--- DirtyDogs/Screens/Game/GameView.swift
+++ DirtyDogs/Screens/Game/GameView.swift
@@ -99,7 +99,6 @@ struct GameView: View {
         .animation(.easeInOut(duration: 0.3), value: viewModel.selectedIndex)
         .onAppear {
             AudioService.shared.playLoopSimple(sound: "MatchSound.mp3", volume: 0.1)
-//            viewModel.onAppear()
         }
         .onDisappear {
             viewModel.onDisappear()
