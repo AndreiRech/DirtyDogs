0908db27599441351524672e649f2bbb076e236a
Júlia Fazenda <juliafazendaruiz@gmail.com>
2025-12-09 17:57:23 -0300

    fix: merge error

--- DirtyDogs/Screens/Game/GameView.swift
+++ DirtyDogs/Screens/Game/GameView.swift
@@ -99,6 +99,7 @@ struct GameView: View {
         .animation(.easeInOut(duration: 0.3), value: viewModel.selectedIndex)
         .onAppear {
             AudioService.shared.playLoopSimple(sound: "MatchSound.mp3", volume: 0.1)
+            viewModel.onAppear()
         }
         .onDisappear {
             viewModel.onDisappear()
